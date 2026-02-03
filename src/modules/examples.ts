4df29726fa9511bbd916846beeb174deed629abc
Hirusha Fernando <heshanhfernando@gmail.com>
2026-02-04 04:21:47 +0530

    feat: add Zotero API integration examples and an empty hooks file.

--- src/modules/examples.ts
+++ src/modules/examples.ts
@@ -144,7 +144,7 @@ export class UIExampleFactory {
       tag: "menuitem",
       id: "zotero-itemmenu-addontemplate-test",
       label: getString("menuitem-label"),
-      commandListener: (ev) => addon.hooks.onClickExportPDF(ev),
+      commandListener: (_) => addon.hooks.onClickExportPDFs(),
       icon: menuIcon,
     });
   }
